9be0ab48f25975f9cadcef8ab67e2fe52a354c9c
Sinthia Katherine Rodriguez Valentin <ktsk@live.com>
2018-08-29 21:23:50 -0500

    Se ajusta los campos requeridos para no generar error en las actualizaciones

--- WebApiCap/WebApiCap/Models/Mision.cs
+++ WebApiCap/WebApiCap/Models/Mision.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiCap.Models
 {
     public class Mision
     {
         public int Id { get; set; }
+        [Required]
         public DateTime FechaHora { get; set; }
+        [Required]
         public string Nombre { get; set; }
     }
 }
--- WebApiCap/WebApiCap/Models/RegistroUsersCap.cs
+++ WebApiCap/WebApiCap/Models/RegistroUsersCap.cs
@@ -1,9 +1,13 @@
-﻿namespace WebApiCap.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCap.Models
 {
     public class RegistroUsersCap
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Tpersona { get; set; }
     }
 }
