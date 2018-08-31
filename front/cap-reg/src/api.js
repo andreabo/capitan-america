3a04cce654591cae41f3f145a0fcb1fc2a9db8a3
Carol Bohorquez <cbohorquez@mcclatchyservices.com>
2018-08-30 20:06:50 -0500

    aaa

--- front/cap-reg/src/api.js
+++ front/cap-reg/src/api.js
@@ -1,5 +1,6 @@
-const apiUrl = 'http://localhost:3000/';
+// const apiUrl = 'http://localhost:3000/';
+const apiUrl = 'http://demouniversidadtadeo.azurewebsites.net/api/Mision/';
 
 export const api = {
-  url: apiUrl,
+    url: apiUrl,
 };
--- front/cap-reg/src/screens/list_items.js
+++ front/cap-reg/src/screens/list_items.js
@@ -1,72 +1,80 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
-import { api } from '../api'
+import {api} from '../api'
 
 class ListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {data: []};
-  }
-
-  componentDidMount() {
-    const that=this;
-    axios.get(`${api.url}${this.props.match.params.list}`, {
-    })
-      .then(function (response) {
-        console.log(response);
-        that.setState({ data: response.data });
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-
-  confirmDelete = (id) => {
-    if (window.confirm('Are you sure you wish to delete this item?')){
-      console.log('PANCHO', id);
-      axios.delete(`${api.url}${this.props.match.params.list}/${id}`, {
-      })
-        .then(function (response) {
-          console.log(response);
-          window.location.reload();
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
     }
-  };
 
-  render() {
-    const listItems = this.state.data.map((item) =>
-      <li className='item-list__item' key={item.id.toString()}>
-        {item.name}
-        <button className='item-list__item__delete-button' onClick={(id) => this.confirmDelete(item.id)}>
-          Delete
-        </button>
-      </li>
-    );
+    componentDidMount() {
+        const that = this;
+        // axios.get(`${api.url}${this.props.match.params.list}`, {
+        console.log(`${api.url}` + `RegistroUsuarios`);
+        axios.get(`
+        ${api.url}
+        RegistroUsuarios`, {})
+            .then(function (response) {
+                console.log(response);
+                that.setState({data: response.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
-    return (
-      <div className='page-container'>
-        <h1>
-          Listing all {this.props.match.params.list}
-        </h1>
-        <div className='buttons-grp'>
-          <div className='button'>
-            <Link to={'/'}>Back Home</Link>
-          </div>
-          <div className='button'>
-            <Link to={`/new/${this.props.match.params.list}`}>Add {this.props.match.params.list}</Link>
-          </div>
-        </div>
-        <ul className='item-list'>
-          {listItems}
-        </ul>
-      </div>
-    );
-  }
-}
+    confirmDelete = (id) => {
+        if (window.confirm('Are you sure you wish to delete this item?')) {
+            console.log('PANCHO', id);
+            axios.delete(`${api.url}${this.props.match.params.list} / ${id}`, {})
+                .then(function (response) {
+                    console.log(response);
+                    window.location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    };
 
-export default ListItem;
+    render() {
+        const listItems = this.state.data.map((item) =>
+            <li className='item-list__item' key={item.id.toString()}>
+                {item.name}
+                <button className='item-list__item__delete-button' onClick={(id) => this.confirmDelete(item.id)}>
+                    Delete
+                </button>
+            </li>
+        );
+
+        return (
+            <div className='page-container'>
+                <h1>
+                    Listing all {this.props.match.params.list}
+                </h1>
+                <div className='buttons-grp'>
+                    <div className='button'>
+                        <Link to={'/'}>Back Home</Link>
+                    </div>
+                    <div className='button'>
+                        <Link to={` / new /${this.props.match.params.list}`}>Add {this.props.match.params.list}</
+                            Link>
+                        <
+                        /div>
+                    </div>
+                    <
+                        ul
+                        className='item-list'>
+                        {listItems}
+                    </ul>
+                    <
+                    /div>
+                    )
+                    ;
+                    }
+                    }
+
+                    export default ListItem;
